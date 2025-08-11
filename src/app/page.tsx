6189363ff9e13c8bedf0fcbc1af3da2dd0843d3a
bhumipandey06 <bhumipandey1188@gmail.com>
2025-08-11 17:48:36 +0530

    Defined Schema , relations for db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <div>
-      <SignedOut>
-        {/* <SignInButton /> */}
-        <SignInButton mode="modal">
-          <Button>
-            Sign In
-          </Button>
-          {/* <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-            Sign In
-          </button> */}
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle/>
+    <div className="m-4">
+      <h1>Home Page Content</h1>
+      
     </div>
   );
 }
